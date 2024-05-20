3dd629652103cf9056d8816510acb6ab9e8c9e15
MHEien <markushei@hotmail.no>
2024-05-20 14:40:54 +0200

    Update

--- src/generateTypes.ts
+++ src/generateTypes.ts
@@ -5,7 +5,7 @@ import fs from 'fs';
 import path from 'path';
 import dotenv from 'dotenv';
 
-dotenv.config({ path: path.resolve(process.cwd(), '.env') });
+dotenv.config({ path: '../../../.env' });
 
 interface Attribute {
   key: string;
